e7278f21c93b0c81afadb46888d961b179d21a60
Nikola Milinkovic <n.milinkovic@levi9.com>
2020-03-12 12:11:41 +0100

    copyright header

--- Nuclear.Channels/Messaging/Services/ChannelMessage/ChannelMessageOutputWriter.cs
+++ Nuclear.Channels/Messaging/Services/ChannelMessage/ChannelMessageOutputWriter.cs
@@ -1,4 +1,8 @@
 ﻿using Nuclear.Channels.Base;
+// Copyright © Nikola Milinkovic 
+// Licensed under the MIT License (MIT).
+// See License.md in the repository root for more information.
+
 using Nuclear.Channels.Hosting.Contracts;
 using Nuclear.ExportLocator;
 using Nuclear.ExportLocator.Decorators;
