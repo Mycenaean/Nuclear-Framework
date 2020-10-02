fe3fce744b32867bd77366b0cfd3ddb8aec59b0d
Nikola Milinkovic <mycenaean93@yahoo.com>
2020-10-02 21:32:54 +0200

    Added capability to return standard json output instead of ChannelMessage

---
+++ Nuclear.Channels.Messaging/StandardJsonMessageAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nuclear.Channels.Messaging
+{
+    /// <summary>
+    /// Decorated Channel Method or response object will not be serialized into ChannelMessage
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public sealed class StandardJsonMessageAttribute : Attribute
+    {
+    }
+}
